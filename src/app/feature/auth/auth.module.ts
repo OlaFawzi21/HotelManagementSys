e16d6b31aff03631b43288626328c69ef2447b87
Ola Fawzi <olafawzii80@gmail.com>
2024-09-08 09:38:26 +0300

    update login

--- src/app/feature/auth/auth.module.ts
+++ src/app/feature/auth/auth.module.ts
@@ -7,6 +7,7 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ForgetPasswordComponent } from './components/forget-password/forget-password.component';
 import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
@@ -19,7 +20,8 @@ import { ResetPasswordComponent } from './components/reset-password/reset-passwo
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    SharedModule
   ]
 })
 export class AuthModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChangePasswordComponent } from './components/change-password/change-password.component';
-
-
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
-  declarations: [
-    ChangePasswordComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ChangePasswordComponent],
+  imports: [CommonModule, InputTextModule, PasswordModule, ButtonModule],
+  exports: [InputTextModule, PasswordModule, ButtonModule],
 })
-export class SharedModule { }
+export class SharedModule {}
