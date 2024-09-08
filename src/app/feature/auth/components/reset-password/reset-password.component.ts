7115ed4ddf29ea5f834999dd65cb7b6dfbd5f24f
dessam <dessam@kn-it.com>
2024-09-08 14:51:32 +0300

    reset password and enhance code

--- src/app/feature/auth/components/reset-password/reset-password.component.ts
+++ src/app/feature/auth/components/reset-password/reset-password.component.ts
@@ -1,10 +1,59 @@
 import { Component } from '@angular/core';
 
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { Router } from '@angular/router';
+
+import { MessageService } from 'primeng/api';
+import { AuthService } from '../../services/auth.service';
+
+import { ResetPasswordRequest } from '../../interfaces/reset-password-request';
+
 @Component({
   selector: 'app-reset-password',
   templateUrl: './reset-password.component.html',
-  styleUrls: ['./reset-password.component.scss']
+  styleUrls: ['./reset-password.component.scss'],
 })
 export class ResetPasswordComponent {
+  loading: boolean = false;
 
+  resetPasswordForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    seed: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+    confirmPassword: new FormControl('', [Validators.required]),
+  });
+
+  constructor(
+    private _auth: AuthService,
+    private messageService: MessageService,
+    private router: Router
+  ) {}
+
+  submit(): void {
+    this.resetPasswordForm.markAllAsTouched();
+
+    if (this.resetPasswordForm.valid) {
+      this._auth
+        .resetPassword(this.resetPasswordForm.value as ResetPasswordRequest)
+        .subscribe({
+          next: (response) => {
+            this.messageService.add({
+              severity: 'success',
+              summary: 'Success',
+              detail: response?.message ?? '',
+            });
+
+            this.router.navigateByUrl('auth/reset-password');
+          },
+          error: (error) => {
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Fail',
+              detail: error.error?.message ?? '',
+            });
+          },
+        });
+    }
+  }
 }
--- src/app/feature/auth/services/auth.service.ts
+++ src/app/feature/auth/services/auth.service.ts
@@ -3,15 +3,31 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+import { ResetPasswordRequest } from '../interfaces/reset-password-request';
+import { ResetPasswordResponse } from '../interfaces/reset-password-response';
+import { ForgetPasswordResponse } from '../interfaces/forget-password-response';
+
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  authBaseUrl = 'portal/users/forgot-password';
+  authBaseUrl = 'portal/users/';
 
   constructor(private _http: HttpClient) {}
 
-  forgetPassword(email: string): Observable<any> {
-    return this._http.post(this.authBaseUrl, email);
+  forgetPassword(email: string): Observable<ForgetPasswordResponse> {
+    return this._http.post<ForgetPasswordResponse>(
+      this.authBaseUrl + 'forgot-password',
+      email
+    );
+  }
+
+  resetPassword(
+    formValue: ResetPasswordRequest
+  ): Observable<ResetPasswordResponse> {
+    return this._http.post<ResetPasswordResponse>(
+      this.authBaseUrl + 'reset-password',
+      formValue
+    );
   }
 }
