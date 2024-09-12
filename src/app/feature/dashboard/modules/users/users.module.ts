ecec29341f700c384aea096b70253c5ca1189a20
reemWael123 <reemelkazaz@gmail.com>
2024-09-12 17:55:09 +0300

    first

--- src/app/feature/dashboard/modules/users/users.module.ts
+++ src/app/feature/dashboard/modules/users/users.module.ts
@@ -3,15 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+
 
 
 @NgModule({
   declarations: [
-    UsersComponent
+    UsersComponent,
+    UserListComponent
   ],
   imports: [
     CommonModule,
-    UsersRoutingModule
+    UsersRoutingModule,
+   SharedModule
   ]
 })
 export class UsersModule { }
