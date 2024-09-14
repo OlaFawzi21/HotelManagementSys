ff4d26ff4b7dd2da4b632104c7a71f39f5935d48
dessam <dessam@kn-it.com>
2024-09-14 16:19:20 +0300

    init landing navbars

---
+++ src/app/feature/landing/components/anonymous-navbar/anonymous-navbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-anonymous-navbar',
+  templateUrl: './anonymous-navbar.component.html',
+  styleUrls: ['./anonymous-navbar.component.scss']
+})
+export class AnonymousNavbarComponent {
+
+}
--- src/app/feature/landing/landing.module.ts
+++ src/app/feature/landing/landing.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { LandingRoutingModule } from './landing-routing.module';
 import { LandingComponent } from './landing.component';
+import { UserNavbarComponent } from './components/user-navbar/user-navbar.component';
+import { AnonymousNavbarComponent } from './components/anonymous-navbar/anonymous-navbar.component';
 
 
 @NgModule({
   declarations: [
-    LandingComponent
+    LandingComponent,
+    UserNavbarComponent,
+    AnonymousNavbarComponent
   ],
   imports: [
     CommonModule,
