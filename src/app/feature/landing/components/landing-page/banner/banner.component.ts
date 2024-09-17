1f59b93846c97b6fb11dc4fca73131b79a47af31
dessam <dessam@kn-it.com>
2024-09-17 10:44:25 +0300

    init banner and navs ui and localization

--- src/app/feature/landing/components/landing-page/banner/banner.component.ts
+++ src/app/feature/landing/components/landing-page/banner/banner.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+import { TranslateService } from '@ngx-translate/core';
+
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.scss'],
 })
 export class BannerComponent {
-  rangeDates: any;
+  rangeDates: any[];
+  capacity: number = 1;
+
+  constructor(public _translate: TranslateService) {}
+
+  test() {
+    console.log('rangeDates', this.rangeDates);
+  }
+
+  decrementCapacity(): void {
+    if (this.capacity > 1) {
+      this.capacity--;
+    }
+  }
+
+  incrementCapacity(): void {
+    this.capacity++;
+  }
 }
